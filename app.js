88a33188a975cdb31ee2ca154529db75bb9570c3
IB_Student <student@InnovateBhams-MacBook-Pro-3.local>
2020-01-15 15:17:10 -0600

    second

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-let resume = {
-    Name: 'Name: Ryan',
-    Career: 'Computers',
-    Description: 'Apple'
-};
+// const resume = {
+//             Name: 'Name: Ryan',
+//         Career: 'Career: Software Developer',
+//         Description:'this is a description',
+//         interests:'football, coding, other interesting things',
+//         PreviousExperience: 'test'}
 
-let allCaps = resume.Name.toUpperCase('Ryan');
+//name, career field, description, interests, past positions,
+//description of jobs, list of skills
 
-console.log(resume.name resume.Career resume.Description);
+
+let name = 'Ryan';
+let career = 'Software Dev';
+let aboutMe = 'i do stuff';
+let pastPos = 'Videographer';
+let skills = 'videos, ographing, vibing';
+let ph = 'Place Holder';
+
+function displayPosition(companyName, jobTitle, description) {
+    console.log('Name: ' + companyName.toUpperCase(), 'Career: ' + jobTitle, 'Description: ' + description)
+}
+
+function displaySkill(skill, isCool) {
+    if (isCool === true) {
+        console.log("BAM: " + skill)
+    } else {
+        console.log(skill)
+    }
+}
+
+console.log('Name: ' + name.toUpperCase());
+console.log('Career: ' + career);
+console.log('Description: I Am a software developer.' + '\n');
+console.log("My Interests:");
+console.log("* Videography");
+console.log("* Swimming" +  '\n');
+displayPosition("Publix", 'clerk', "do stuff");
+displayPosition("Target", "CEO", "sweep" + '\n');
+console.log('My Skills:');
+displaySkill("Playdough", false);
+displaySkill("Pilot", true);
