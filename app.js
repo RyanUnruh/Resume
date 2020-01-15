17c87538445ffc7ba1a3d3184c398aeb94d8ead9
IB_Student <student@InnovateBhams-MacBook-Pro-3.local>
2020-01-15 10:55:57 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let resume = {
+    Name: 'Name: Ryan',
+    Career: 'Computers',
+    Description: 'Apple'
+};
+
+let allCaps = resume.Name.toUpperCase('Ryan');
+
+console.log(resume.name resume.Career resume.Description);
